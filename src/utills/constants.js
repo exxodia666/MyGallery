60906f695e54d1b4ee349b23661aa425d6bda7d0
Alexey <42065561+exxodia666@users.noreply.github.com>
2020-09-22 23:14:16 +0300

    Create constants.js

---
+++ src/utills/constants.js
@@ -0,0 +1,6 @@
+export const status = {
+  pending: 'pending',
+  done: 'done',
+  error: 'error',
+};
+export const url = 'https://api.unsplash.com/photos/?client_id=iWj0huca0DBEMYJQ5Sx9TugUzLfIBNLzlO7yDKRVX9g';
