279b4d877e641a4caca93cf614dcbc4b535870be
Alexey <42065561+exxodia666@users.noreply.github.com>
2020-09-22 23:14:13 +0300

    render pictures

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,27 +1,55 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
+import {observer} from 'mobx-react';
+//import {observer} from 'mobx';
+//import {inject} from 'react-mobx';
+import React, {useEffect} from 'react';
+import {Alert, StyleSheet, Text, View} from 'react-native';
+import {ScrollView, TouchableOpacity} from 'react-native-gesture-handler';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import Post from '../components/Post';
+import RootModel from '../models/RootModel';
+import {status} from '../utills/constants';
+const url = 'https://reactnative.dev/img/tiny_logo.png';
+const descr = 'Kekwait picture here';
+const id = 3124124;
 
-const Home = () => {
+const Home = (props) => {
+  const pictures = RootModel.picturesStore.pictures;
+  const state = RootModel.picturesStore.state;
+  console.log(pictures[5].user.name);
+  console.log(pictures[5].urls.full);
+  useEffect(() => {
+    RootModel.picturesStore.savePictures();
+  }, [state]);
 
-    const url = 'https://reactnative.dev/img/tiny_logo.png';
-    const descr = 'Kekwait picture here';
-    const id = 3124124;
-    return (<View style={styles.container}>
-        <Icon name='home' size={80} color='black' />
-        <Text style={{ fontSize: 26 }}>Home</Text>
+  if (state === status.pending) {
+    <Text>Loading!!!</Text>;
+  } else if (state === status.done) {
+    return (
+      <View style={styles.container}>
         <ScrollView>
-            <Post url={url} descr={descr} id={id} />
+          {pictures.map((pic) => {
+            return (
+              <Post
+                url={pic.urls.full}
+                descr={pic.description || pic.alt_description}
+                id={pic.id}
+                author={pic.user.name}
+              />
+            );
+          })}
         </ScrollView>
-    </View>);
+      </View>
+    );
+  } else if (state === status.error) {
+    <Text>error</Text>;
+  }
 };
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: "center",
-    }
-})
-export default Home;
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default observer(Home);
