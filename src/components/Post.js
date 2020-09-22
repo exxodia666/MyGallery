cb5b890d54247e74beeaf2a05e3a748378457003
Alexey <42065561+exxodia666@users.noreply.github.com>
2020-09-22 23:13:21 +0300

    few changes in store

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,22 +1,23 @@
 import React from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import {Image, StyleSheet, Text, View} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
-const Post = ({ url, descr, id }) => {
-    return (<View style={styles.container}>
-        <Image source={{ uri: url }} style={{ width: 200, height: 200 }} />
-        <Text>
-            {descr}
-        </Text>
-    </View>);
+const Post = ({url, descr, id, author}) => {
+  return (
+    <View style={styles.container}>
+      <Image source={{uri: url}} style={{width: '100%', height: 200}} />
+      <Text>{descr}</Text>
+      <Text>Created by {author}</Text>
+    </View>
+  );
 };
 const styles = StyleSheet.create({
-    container: {
-        padding: 10,
-        //backgroundColor: 'red',
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: "center",
-    }
-})
+  container: {
+    marginBottom: 20,
+    elevation: 4,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 export default Post;
--- src/models/PicListModel.js
+++ src/models/PicListModel.js
@@ -2,47 +2,23 @@ import Axios from 'axios';
 
 //import { persist } from 'mobx-persist';
 const {decorate, observable, action, computed} = require('mobx');
-
-const status = {
-  pending: 'pending',
-  done: 'done',
-  error: 'error',
-};
-/*
-const url = `https://api.unsplash.com/photos/?client_id=${key}`;
-const response = await fetch(url);
-export const fetchPosts = () => {
-  return async (dispatch) => {
-    try {
-      dispatch(showLoader());
-      const response = await fetch(url);
-      const json = await response.json();
-      setTimeout(() => {
-        dispatch({type: 'FETCH_POSTS', payload: json});
-        dispatch(hideLoader());
-      }, 1000);
-    } catch (e) {
-      console.log('You lose');
-      return null;
-    }
-  };
-};
-*/
-//const key = 'iWj0huca0DBEMYJQ5Sx9TugUzLfIBNLzlO7yDKRVX9g';
-//const url = `https://api.unsplash.com/photos/?client_id=${key}`;
-const url =
-  'https://api.unsplash.com/photos/?client_id=iWj0huca0DBEMYJQ5Sx9TugUzLfIBNLzlO7yDKRVX9g';
-
+import {url, status} from '../utills/constants';
 const fetchPictures = () => {
-  Axios.get(url).then((res) => {
-    console.log(JSON.stringify(res));
-  });
+  return Axios.get(url);
 };
 
 class PicListModel {
   state = status.pending;
-  savePictures = (items) => {
-    fetchPictures();
+  pictures = [];
+
+  savePictures = () => {
+    fetchPictures().then(
+      (res) => {
+        this.pictures = res.data.map((pic) => pic);
+        this.state = status.done;
+      },
+      (error) => (this.state = status.error),
+    );
   };
 }
 
